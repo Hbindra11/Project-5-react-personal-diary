01c1e59d8277c09d26636fe5fa16d0070aae4fe1
Hema Bindra <hbindra11@yahoo.com>
2024-10-25 13:12:36 +0200

    added AddEntryModal component and mechanism to open it through App.jsx by means of a button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+import AddEntryModal from "./components/AddEntryModal";
 
+const App = () => {
+  // const [count, setCount] = useState(0);
+  //const today = JSON.stringify(new Date()).substring(1,11);
   return (
     <>
-      <div className=" bg-zinc-200">
-        <h1 className="text-3xl font-bold text-center">Personal Diary</h1>
+     
+      <div className=" bg-zinc-200"></div>
+      <h1 className="text-3xl font-bold text-center">Personal Diary</h1>
+      <div className="flex justify-end pr-20">
+        <button
+          className="btn"
+          onClick={() => document.getElementById("my_modal_5").showModal()}
+        >
+          Add A New Entry
+        </button>
       </div>
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <AddEntryModal />
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </>
   );
-}
+};
 
 export default App;
